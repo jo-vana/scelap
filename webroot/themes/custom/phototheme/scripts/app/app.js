9bee4c463279bd6857cbae5aeeaad6cf1c208702
jo-vana <jovananukic85@gmail.com>
2018-02-19 09:01:23 +0100

    Theming in proccess(starting responsive)

--- webroot/themes/custom/phototheme/scripts/app/app.js
+++ webroot/themes/custom/phototheme/scripts/app/app.js
@@ -2,4 +2,39 @@
 
 	'use strict';
 
+	// Back to top button
+
+    Drupal.behaviors.backToTop = {
+        attach:function(context) {
+            var btn = $('#block-backtotop a');
+
+            $(window).scroll(function() {
+                if ($(window).scrollTop() > 100) {
+                    btn.addClass('show');
+                } else {
+                    btn.removeClass('show');
+                }
+            });
+
+            btn.on('click', function(e) {
+                e.preventDefault();
+                $('html, body').animate({scrollTop:0}, '100');
+            });
+        }
+    };
+
+    // Album page slider
+
+    Drupal.behaviors.aboutPageBackground = {
+        attach:function() {
+
+        }
+    };
+
+
+
+
+
+
+
 })(jQuery);
